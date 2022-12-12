e62551ed6475cfc4cb87e9ef6090a8549bd30253
박상준 <betterforwhat@encar.com>
2022-12-12 18:06:51 +0900

    Q&A 마무리

--- prepare/back/models/comment.js
+++ prepare/back/models/comment.js
@@ -1,3 +1,33 @@
+// // 클래스 최신 문법
+// const DataTypes = require('sequelize');
+// const { Model } = DataTypes;
+
+// module.exports = class Comment extends Model {
+//   static init(sequelize) {
+//     return super.init({
+//       content: {
+//         type: DataTypes.TEXT,
+//         allowNull: false,
+//       }
+//     }, {
+//       modelName: 'Comment',
+//       tableName: 'comments',
+//       charset: 'utf8mb4',
+//       collate: 'utf8mb4_general_ci',
+//       sequelize,
+//     });
+//   }
+
+//   static assciate(db) {
+//     db.Comment.belongsTo(db.User);
+//     db.Comment.belongsTo(db.Post);
+//   }
+// };
+
+
+// 기존 문법
+
+
 module.exports = (sequelize, DataTypes) => {
   const Comment = sequelize.define('Comment', {
     content: {
--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 import shortId from 'shortid';
-import produce from 'immer';
+import produce from '../util/produce';
 
 export const initialState = {
   mainPosts: [],
